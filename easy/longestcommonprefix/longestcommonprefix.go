2ba5ab7121ac5e5c3e1b8354fb20c0fb49e56e45
Vivek Kumar <vickythebest008@gmail.com>
2023-05-03 18:35:16 -0700

    longest common prefix in array

---
+++ easy/longestcommonprefix/longestcommonprefix.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"fmt"
+)
+
+func LongestCommonPrefix(strs []string) string {
+
+	fmt.Println(strs)
+	if len(strs) <= 0 {
+		return ""
+	}
+
+	fmt.Println("arrya is not null", strs)
+	prefix := ""
+	// count := 0
+	ch1 := ""
+	for i := 0; i < len(strs[0]); i++ {
+		fmt.Println("  continue with new i ", i)
+		// if len(strs[0]) > 0 {
+		fmt.Println("exit ", i)
+		ch1 = string(strs[0][i])
+		// }
+
+		// fmt.Println("  char = ", ch1)
+		for j := 1; j < len(strs); j++ {
+			fmt.Println("inside second loop ", j, len(strs[j]))
+			// fmt.Println("   J = ", strs[j])
+			// fmt.Println("   I = ", strs[i])
+			// fmt.Printf("  %v %v \n", i, j)
+			if i >= len(strs[j]) || ch1 != string(strs[j][i]) {
+				fmt.Println("Exit")
+				return prefix
+			}
+		}
+		prefix = prefix + ch1
+	}
+	fmt.Println("  for done ")
+	return prefix
+}
+
+// func LongestCommonPrefix(strs []string) string {
+
+// 	fmt.Println(strs)
+
+// 	if len(strs) <= 0 {
+// 		return ""
+// 	}
+// 	match := ""
+// 	left := 0
+
+// 	if len(strs) > 1 {
+// 		chars := []rune(strs[0])
+// 		prefix := ""
+// 		if len(chars) > 1 {
+// 			prefix = string(chars[0])
+// 		}
+
+// 		for i := range strs {
+// 			chars1 := []rune(strs[i])
+// 			pre := ""
+// 			if len(chars1) > i {
+// 				pre = string(chars1[i])
+// 			}
+
+// 			if prefix == pre {
+// 				match = match + prefix
+// 				left++
+// 			}
+// 			if len(chars1) > left {
+// 				prefix = string(chars1[left])
+// 			}
+// 		}
+// 	} else {
+// 		return strs[0]
+// 	}
+
+// 	if left > 0 {
+// 		return match
+// 	} else {
+// 		return ""
+// 	}
+
+// }
