43c05b1a3a48efc1919b978515554090bdb163e9
Vivek Kumar <vickythebest008@gmail.com>
2023-05-08 15:04:20 -0700

    correct the variable

--- easy/leetcode-28/firstOccurrenceinString.go
+++ easy/leetcode-28/firstOccurrenceinString.go
@@ -8,13 +8,9 @@ import (
 func StrStr(haystack string, needle string) int {
 	fmt.Println("first Occurene in string")
 
-	// match := -1
-	// index := 0
-	// left := 0
-	result := -1
 	index := strings.Index(haystack, needle)
 
 	fmt.Println(index)
 
-	return result
+	return index
 }
