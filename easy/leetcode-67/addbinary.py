bc20bb3851df11be5ebca1bc083abd5b6b3ea8b3
vivek kumar <vickythebest008@gmail.com>
2023-07-26 15:20:09 -0700

    leetcode-67

--- easy/leetcode-67/addbinary.py
+++ easy/leetcode-67/addbinary.py
@@ -9,25 +9,43 @@ class Solution(object):
         result="0"
 
         while (a1 >= 0) or (b1 >=0):
-            # print(a1," <-index-> ",b1)
-            # print(a[a1]," <-value-> ",b[b1])
-
-            carry=int(a[a1])+int(b[b1])
-            # print("carry : ",carry)
-
-            if carry %2==0:
-
+            sum=0
+            # print("Reset : ",sum)
+            print("int(a[a1]) : ",int(a[a1]),"  __  int(b[b1] : ",int(b[b1]))
+            sum=int(a[a1])+int(b[b1])
+            print("Sum = ", sum, "carry : ",carry)
+            if sum == 2 and carry==1:
+                result+= "1"
+                carry=0
+            elif sum ==2 and carry==0:
                 result+= "0"
+                carry=1
+            elif sum ==1 and carry==1:
+                result+="0"
+                carry=0
+            elif sum==1 and carry==0:
+                result+="1"
+                carry=0
+            elif sum==0 and carry==1:
+                result+="1"
+                carry=0
+            # else:
+            #     result="1"
+            # if carry %2==0:
                 
-                print("if ",result)
-            else:
-                
-                result=result+str(carry)
-                print("else ",result)
+            #     print("if ",result)
+            #     carry=1
+            # else:
+            #     result=result+str(carry)
+            #     print("else ",result)
 
-            print("Output : ", result)
+
+            print("Result : ", result)
+            # print("old carry : ", carry)
             a1=a1-1
             b1=b1-1
+        if carry >0:
+            result+="1"
 
         return result
         """
@@ -37,10 +55,10 @@ class Solution(object):
         """
 
 s1=Solution()
-# print("Result",s1.addBinary("101","11"))
-print("Call",s1.addBinary("1010","1011"))
+print("Result",s1.addBinary("101","11"))
+# print("Call",s1.addBinary("1010","1011"))
 
 # 101  1010
 #  11  1011
-# ---------
+# ---  ----
 # 100 10101
